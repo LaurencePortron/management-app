30cab9bb0cb0adf8ccacb49b36b77a144f8415e5
LaurencePortron <portron.laurence@gmail.com>
2020-11-15 19:50:36 +0100

    basics set

---
+++ src/components/TaskListNavigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function TaskListNavigation() {
+  return (
+    <>
+      <div className='tasklist-navigation'>
+        <div className='task-list'></div>
+        <div className='status-list'></div>
+        <div className='timeline-list'></div>
+      </div>
+    </>
+  );
+}
